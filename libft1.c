5bddae08d9a56378ab5a82dd188067df05e8a179
cephei14 <rafik.dhaibi@gmail.com>
2025-02-20 16:09:29 +0900

    Restore deleted files from GitHub

--- libft1.c
+++ libft1.c
@@ -6,7 +6,7 @@
 /*   By: rdhaibi <rdhaibi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 15:03:51 by rdhaibi           #+#    #+#             */
-/*   Updated: 2025/02/17 19:15:28 by rdhaibi          ###   ########.fr       */
+/*   Updated: 2025/02/20 16:06:43 by rdhaibi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,20 @@ int	sign(const char *str, long int *i)
 	{
 		(*i)++;
 		if (!ft_isdigit((char)str[*i]))
-		return (0);
+			return (0);
 		if (str[*i - 1] == '-')
-		return (-1);
+			return (-1);
 	}
 	return (1);
 }
 
-int ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
 	if (s1[i] == '+')
 		i++;
 	if (s2[j] == '+')
@@ -43,18 +45,18 @@ int ft_strcmp(char *s1, char *s2)
 		i++;
 	while (s2[j] == '0')
 		j++;
-	while((s1[i] == s2[j]) && s1[i] && s2[j])
+	while ((s1[i] == s2[j]) && s1[i] && s2[j])
 	{
 		i++;
 		j++;
 	}
-	return (s1[i]-s2[j]);
+	return (s1[i] - s2[j]);
 }
 
 int	lenn(const char *str, long int i)
 {
 	long int	l;
-	
+
 	l = 1;
 	while (ft_isdigit(str[i]))
 	{
@@ -78,28 +80,34 @@ int	assign(const char *str, long int i, long int l)
 	return (n);
 }
 
-long ft_atoi(const char *str)
+long	ft_atoi(const char *str)
 {
-    long result = 0;
-    int sign = 1;
-    int i = 0;
+	long	result;
+	int		sign;
+	int		i;
 
-    while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
-        i++;
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        if (result > (LONG_MAX - (str[i] - '0')) / 10)
-        {
-            return (sign == 1 ? LONG_MAX : LONG_MIN);
-        }
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return (result * sign);
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (result > (LONG_MAX - (str[i] - '0')) / 10)
+		{
+			if (sign == 1)
+				return (LONG_MAX);
+			else
+				return (LONG_MIN);
+		}
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
--- push_swap.h
+++ push_swap.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   0-push_swap.h                                      :+:      :+:    :+:   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdhaibi <rdhaibi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 17:03:41 by rdhaibi           #+#    #+#             */
-/*   Updated: 2025/02/17 16:47:18 by rdhaibi          ###   ########.fr       */
+/*   Updated: 2025/02/17 22:50:22 by rdhaibi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,75 +17,58 @@
 # include <stdlib.h>
 # include <limits.h>
 
-#include <stdio.h> //test
-
 typedef struct stack
 {
-    struct stack *previous;
-    int         data;
-    struct stack *next;
-}   t_stack;
-
-//libft
-int	sign(const char *str, long int *i);
-int	ft_strcmp(char *s1, char *s2);
-int	lenn(const char *str, long int i);
-int	assign(const char *str, long int i, long int l);
+	struct stack	*previous;
+	int				data;
+	struct stack	*next;
+}	t_stack;
+int		sign(const char *str, long int *i);
+int		ft_strcmp(char *s1, char *s2);
+int		lenn(const char *str, long int i);
+int		assign(const char *str, long int i, long int l);
 long	ft_atoi(const char *str);
 char	*edges(char const *s, char c, int *i);
-int	wordsnum(char const *s, char c);
-char    **ft_split(char const *s, char c);
+int		wordsnum(char const *s, char c);
+char	**ft_split(char const *s, char c);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s1);
-int	ft_isdigit(int c);
+int		ft_isdigit(int c);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-//libft
-
-//linked list
-t_stack *create_node(int data);
-t_stack *add_node_top(t_stack *stack_ptr, int data);
-t_stack *add_node_bottom(t_stack *stack_ptr, int data);
-//linked list
-
-//remove
-t_stack *remove_head(t_stack *stack_ptr);
-t_stack *remove_tail(t_stack *stack_ptr);
-void    delete_node(t_stack *node);
-void free_stack(t_stack *stack);
-void free_args(char **args);
-//remove
-
-//operations
-void    swap(t_stack *stack_ptr);
-void    rotate(t_stack **stack_ptr);
-void    reverse_rotate(t_stack **stack_ptr);
-void    pa(t_stack **stack_a, t_stack **stack_b);
-void    pb(t_stack **stack_a, t_stack **stack_b);
-void    sa(t_stack *stack_a);
-void    sb(t_stack *stack_b);
-void    ss(t_stack *stack_a, t_stack *stack_b);
-void    ra(t_stack **stack_a);
-void    rb(t_stack **stack_b);
-void    rr(t_stack **stack_a, t_stack **stack_b);
-void    rra(t_stack **stack_a);
-void    rrb(t_stack **stack_b);
-void    rrr(t_stack **stack_a, t_stack **stack_b);
-//operations
-
-//sorting
-void push_swap(t_stack **a, t_stack **b);
-int list_size(t_stack *stack);
-int get_rank(t_stack *stack, int value);
-void normalize_stack(t_stack *stack);
-int find_max(t_stack *stack);
-int find_min(t_stack *stack);
-void sort_three(t_stack **a);
-void sort_small(t_stack **a, t_stack **b);
-void sort_100_numbers(t_stack **a, t_stack **b);
-int position_of(t_stack *stack, int value);
-//sorting
-void print_stack(t_stack *stack);
-int is_sorted(t_stack *stack);
+t_stack	*create_node(int data);
+t_stack	*add_node_top(t_stack *stack_ptr, int data);
+t_stack	*add_node_bottom(t_stack *stack_ptr, int data);
+t_stack	*remove_head(t_stack *stack_ptr);
+t_stack	*remove_tail(t_stack *stack_ptr);
+void	delete_node(t_stack *node);
+void	free_stack(t_stack *stack);
+void	free_args(char **args);
+void	swap(t_stack *stack_ptr);
+void	rotate(t_stack **stack_ptr);
+void	reverse_rotate(t_stack **stack_ptr);
+void	pa(t_stack **stack_a, t_stack **stack_b);
+void	pb(t_stack **stack_a, t_stack **stack_b);
+void	sa(t_stack *stack_a);
+void	sb(t_stack *stack_b);
+void	ss(t_stack *stack_a, t_stack *stack_b);
+void	ra(t_stack **stack_a);
+void	rb(t_stack **stack_b);
+void	rr(t_stack **stack_a, t_stack **stack_b);
+void	rra(t_stack **stack_a);
+void	rrb(t_stack **stack_b);
+void	rrr(t_stack **stack_a, t_stack **stack_b);
+void	push_swap(t_stack **a, t_stack **b);
+int		list_size(t_stack *stack);
+int		get_rank(t_stack *stack, int value);
+void	normalize_stack(t_stack *stack);
+int		find_max(t_stack *stack);
+int		find_min(t_stack *stack);
+void	sort_three(t_stack **a);
+void	sort_small(t_stack **a, t_stack **b);
+void	sort_100_numbers(t_stack **a, t_stack **b);
+int		position_of(t_stack *stack, int value);
+void	print_stack(t_stack *stack);
+int		is_sorted(t_stack *stack);
 
 #endif
