ab2a927644b974e48932722850b882872f85423a
cephei14 <rafik.dhaibi@gmail.com>
2025-02-20 17:23:07 +0900

    Yo

--- 0-push_swap.c
+++ 0-push_swap.c
@@ -6,135 +6,136 @@
 /*   By: rdhaibi <rdhaibi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 17:17:57 by rdhaibi           #+#    #+#             */
-/*   Updated: 2025/02/17 23:04:37 by rdhaibi          ###   ########.fr       */
+/*   Updated: 2025/02/20 17:22:04 by rdhaibi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int arg_check(int count, char **av)
+int	arg_check(int count, char **av)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
+	long	num;
 
-    i = 0;
-    while (i < count)
-    {
-        j = 0;
-        if (av[i][j] == '+' || av[i][j] == '-')
-        {
-            j++;
-            if (av[i][j] == '\0')
-                return (0);
-        }
-        while (av[i][j])
-        {
-            if (av[i][j] < '0' || av[i][j] > '9')
-                return (0);   
-            j++;
-        }
-        long num = ft_atoi(av[i]);
-        if (num == LONG_MAX || num == LONG_MIN || num > INT_MAX || num < INT_MIN)
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (i < count)
+	{
+		j = 0;
+		if (av[i][j] == '+' || av[i][j] == '-')
+		{
+			j++;
+			if (av[i][j] == '\0')
+				return (0);
+		}
+		while (av[i][j])
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (0);
+			j++;
+		}
+		num = ft_atoi(av[i]);
+		if (num == LONG_MAX || num == LONG_MIN
+			|| num > INT_MAX || num < INT_MIN)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int dupl_check(int count, char **av)
+int	dupl_check(int count, char **av)
 {
-    int i;
-    int j;
-    
-    i = 0;
-    while (i < count)
-    {
-        j = i + 1;
-        while (j < count)
-        {
-            if (ft_atoi(av[i]) ==ft_atoi(av[j]))
-                return (1);
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < count)
+	{
+		j = i + 1;
+		while (j < count)
+		{
+			if (ft_atoi(av[i]) == ft_atoi(av[j]))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
-int is_sorted(t_stack *stack)
+int	is_sorted(t_stack *stack)
 {
-    while (stack && stack->next)
-    {
-        if (stack->data > stack->next->data)
-            return 0;
-        stack = stack->next;
-    }
-    return 1;
+	while (stack && stack->next)
+	{
+		if (stack->data > stack->next->data)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
 }
 
-void push_swap(t_stack **a, t_stack **b)
+void	push_swap(t_stack **a, t_stack **b)
 {
-    if (is_sorted(*a))
-        return;
+	int	size;
 
-    int size = list_size(*a);
-
-    if (size <= 10)
-    {
-        sort_small(a, b);
-    }
-    else
-    {
-        sort_100_numbers(a, b);
-    }
+	if (is_sorted(*a))
+		return ;
+	size = list_size(*a);
+	if (size <= 10)
+		sort_small(a, b);
+	else
+		sort_100_numbers(a, b);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_stack *a = NULL;
-    t_stack *b = NULL;
-    char **args = NULL;
-    int count;
-    int i;
+	t_stack	*a;
+	t_stack	*b;
+	char	**args;
+	int		count;
+	int		i;
 
-    if (ac == 1)
-        return (0); // No output for no arguments
-    if (ac == 2 && av[1][0] == '\0')
-        return (0); // No output for empty string argument
-    if (ac == 2)
-    {
-        args = ft_split(av[1], ' ');
-        if (!args)
-        {
-            write(2, "Error\n", 6);
-            return (-1);
-        }
-        count = 0;
-        while (args[count])
-            count++;
-    }
-    else
-    {
-        args = &av[1];
-        count = ac - 1;
-    }
-    if (!arg_check(count, args) || dupl_check(count, args))
-    {
-        write(2, "Error\n", 6);
-        if (ac == 2)
-            free_args(args);
-        return (-1);
-    }
-    i = 0;
-    while (i < count)
-    {
-        a = add_node_bottom(a, ft_atoi(args[i]));
-        i++;
-    }
-    if (ac == 2)
-        free_args(args);
-    push_swap(&a, &b);
-    free_stack(a);
-    free_stack(b);
-    return (0);
+	a = NULL;
+	b = NULL;
+	args = NULL;
+	if (ac == 1)
+		return (0);
+	if (ac == 2 && av[1][0] == '\0')
+		return (0);
+	if (ac == 2)
+	{
+		args = ft_split(av[1], ' ');
+		if (!args)
+		{
+			write(2, "Error\n", 6);
+			return (-1);
+		}
+		count = 0;
+		while (args[count])
+			count++;
+	}
+	else
+	{
+		args = &av[1];
+		count = ac - 1;
+	}
+	if (!arg_check(count, args) || dupl_check(count, args))
+	{
+		write(2, "Error\n", 6);
+		if (ac == 2)
+			free_args(args);
+		return (-1);
+	}
+	i = 0;
+	while (i < count)
+	{
+		a = add_node_bottom(a, ft_atoi(args[i]));
+		i++;
+	}
+	if (ac == 2)
+		free_args(args);
+	push_swap(&a, &b);
+	free_stack(a);
+	free_stack(b);
+	return (0);
 }
--- 3-sorting_1.c
+++ 3-sorting_1.c
@@ -6,73 +6,78 @@
 /*   By: rdhaibi <rdhaibi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:25:30 by rdhaibi           #+#    #+#             */
-/*   Updated: 2025/02/17 19:15:28 by rdhaibi          ###   ########.fr       */
+/*   Updated: 2025/02/20 17:21:04 by rdhaibi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/* Returns the number of nodes in a stack */
-int list_size(t_stack *stack)
+int	list_size(t_stack *stack)
 {
-    int count = 0;
-    while (stack)
-    {
-        count++;
-        stack = stack->next;
-    }
-    return count;
+	int count;
+
+	count = 0;
+	while (stack)
+	{
+		count++;
+		stack = stack->next;
+	}
+	return (count);
 }
 
-/* Returns the number of nodes with data less than 'value' */
-int get_rank(t_stack *stack, int value)
+int	get_rank(t_stack *stack, int value)
 {
-    int rank = 0;
-    while (stack)
-    {
-        if (stack->data < value)
-            rank++;
-        stack = stack->next;
-    }
-    return rank;
+	int	rank;
+
+	rank = 0;
+	while (stack)
+	{
+		if (stack->data < value)
+			rank++;
+		stack = stack->next;
+	}
+	return (rank);
 }
 
-/* Normalize the stack so each value is replaced by its rank (0 .. n-1) */
-void normalize_stack(t_stack *stack)
+void	normalize_stack(t_stack *stack)
 {
-    t_stack *tmp = stack;
-    int orig;
-    while (tmp)
-    {
-        orig = tmp->data;
-        tmp->data = get_rank(stack, orig);
-        tmp = tmp->next;
-    }
+	t_stack *tmp;
+	int		orig;
+
+	tmp = stack;
+	while (tmp)
+	{
+		orig = tmp->data;
+		tmp->data = get_rank(stack, orig);
+		tmp = tmp->next;
+	}
 }
 
-/* Returns the maximum value in the stack */
-int find_max(t_stack *stack)
+int	find_max(t_stack *stack)
 {
-    int max = -1;
-    while (stack)
-    {
-        if (stack->data > max)
-            max = stack->data;
-        stack = stack->next;
-    }
-    return max;
+	int	max;
+
+	max = -1;
+	while (stack)
+	{
+		if (stack->data > max)
+			max = stack->data;
+		stack = stack->next;
+	}
+	return (max);
 }
 
-/* Returns the 0-indexed position of 'value' in the stack */
-int position_of(t_stack *stack, int value)
+int	position_of(t_stack *stack, int value)
 {
-    int pos = 0;
-    while (stack)
-    {
-        if (stack->data == value)
-            return pos;
-        pos++;
-        stack = stack->next;
-    }
-    return pos;
+	int pos;
+
+	pos = 0;
+	while (stack)
+	{
+		if (stack->data == value)
+			return pos;
+		pos++;
+		stack = stack->next;
+	}
+	return (pos);
 }
